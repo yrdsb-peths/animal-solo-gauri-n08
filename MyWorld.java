45d0dc45a75b6b500c9d99e487eddcc98b6feb6c
gauri-n08 <gaurisknair08@gmail.com>
2025-04-30 08:30:24 -0400

    Elephant Updated to Move & Middle of World

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,17 @@
 import greenfoot.*;
 
+/**
+ * The World our hero lives in.
+ * 
+ * @author Gauri
+ * @version April 2025
+ */
+
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
     }
 }
